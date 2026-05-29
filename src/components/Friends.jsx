dbf4ef243063c73ec1a6d3ac68523cf60551bb92
tonmoy-y <tonmoysarker451@gmail.com>
2026-05-29 17:36:49 +0600

    Update README.md file

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -9,7 +9,7 @@ const Friends = () => {
         const friends = use(friendsPromise);
     return (
         <div className='mx-auto text-center container px-8 md:px-30 space-y-4 p-10'>
-            <h2 className='text-left'>Your Friends</h2>
+            <h2 className='text-left font-semibold text-2xl'>Your Friends</h2>
   
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6'>
                 {
