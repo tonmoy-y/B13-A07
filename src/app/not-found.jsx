c1e31841f77d541c41f86c7b9685af0063d6bf77
tonmoy-y <tonmoysarker451@gmail.com>
2026-05-29 16:38:43 +0600

    design custom 404 page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,10 +1,13 @@
+import Link from 'next/link';
 import React from 'react';
+import { RiHome2Line ,RiTimeLine } from "react-icons/ri";
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>404</h2>
-            <h2>Page not found</h2>
+        <div className=' container  flex flex-col items-center my-auto space-y-5'>
+            <h1 className='text-9xl font-semibold'>404</h1>
+            <h2 className='text-6xl'>Page not found</h2>
+            <Link href="/"> <button className='btn btn-neutral text-white font-bold text-xl'><RiHome2Line size={24} /> Back to Home</button> </Link>
         </div>
     );
 };
